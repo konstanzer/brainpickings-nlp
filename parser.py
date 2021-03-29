52031ac765ee857952f2682e61fd9818ab490569
Steve Newton <newton36@gmail.com>
2021-03-29 01:35:18 -0400

    parser

--- parser.py
+++ parser.py
@@ -1,21 +1,61 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-#text = soup.findAll(text=True)
-#headers  = ['style', 'script', '[document]', 'head', 'title']
-#filtered = filter(lambda e: False if e.parent.name in headers else True, text)
-#for s in filtered: print(s)
 
-#print(soup.prettify())
+def extract_articles(data, soup, ix):
+	"""
+	Takes soup and returns the fields of the articles.
+	Args: int
+	Returns: list
+	"""
+	i=0
+	j=len(soup.find_all("h1", {"class": "entry-title"}))
+	
+	for h in soup.find_all("h1", {"class": "entry-title"}):
+		index = ix+j-i
+		title = str(h.text + " - " + h.find_next_sibling().get_text())
+		date = str(h.a)[39:49]
 
-#for link in soup.find_all('a'): print(link.get('href'))
+		tag = soup.find_all(id="posts")[i]
+		tags = list()
+		for child in tag.get('class'):
+			if child[:3] == "tag":
+				tags.append(child[4:])
+		i+=1
 
-#print(soup.get_text())
-    
-if __name__ == "__main__":
-	#All articles as of 3/28/2021
-	for page in range(1, 5):
+		html = u""
+		for tag in h.next_siblings:
+			if str(tag)[:3] in ["<h2","<h3"]: continue
+			if tag == "!--END posts--": break
+			content = str(tag).replace("\n","")
+			html += content
+		html_soup = BeautifulSoup(html, "html.parser")
+		content = html_soup.findAll(text=True)
+
+		data.append(dict(index=index, title=title, date=date, tags=tags, content=content))
+
+	return i, data
+
+
+def article_loader(page_count):
+	"""
+	Loads files individually.
+	Args: int
+	"""
+	ix=0
+	data=[]
+	for page in range (page_count, 0, -1):
 		file = open(f'pages/{page}.txt')
-		text = file.read()
-		soup = BeautifulSoup(text)
-		print(soup.get_text())
+		soup = BeautifulSoup(file.read(), "html.parser")
+		i, data = extract_articles(data, soup, ix)
+		ix+=i
+	df = pd.DataFrame(data)
+	df.to_csv('pages/articles.csv', index=False)
+
+
+if __name__ == "__main__":
+
+	#1425 pages scraped from brainpickings.org
+	page_count = 1425
+	article_loader(page_count)
